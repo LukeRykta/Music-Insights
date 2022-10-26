ba6a00ae0461050a3dced3bded1845a015160668
Luke <80142097+LukeRykta@users.noreply.github.com>
2022-10-25 21:49:48 -0500

    NEW BEARER

--- frontend/src/services/spotifyService.js
+++ frontend/src/services/spotifyService.js
@@ -3,7 +3,7 @@ const apiUrl = 'https://api.spotify.com/v1/'
 
 export const getSpotifyTrack = async () => {
     const url = `${apiUrl}tracks/6AdQ1rD4U0GippNGIm7wJ7?si=ca1d31827f604339`;
-    const token = 'BQC1BuNHFGLMm38VnZkRzIWjahhHskkllU9w-WZYsu5qQibjsVbwx7T-eHwC_IcVSXPBNRzpzcZt3HtMb2dFg219_rO5FBQ6Q5HfnmokBQBrKu0sRkHXIwRCkLs0eix_Kq1LEoQRxwQPGrXjKPF0_xE3U7i3fOcJN7-J6cUa3sWfabaDJRLNLTlvbVkH86Fq'
+    const token = 'BQDCQRcdlFrHGTnrGmKZ2Uahaivpu9-HZf8iyVnS1qWKu8b6ZaGiZanMaKbnD5FBPkN_Vgo-IXi-WLzLmABKUdogzTqNcEn7a_S4wkqwVGMx-j7ETI_cDYK4J0Jx_fJdJy8EMhaLOYJa4LEldIyDvNVDV3mkRiUmWIc5-STkAXaJ6iD_fL6W9tnREfSBrzBa'
     const config = {
         headers: {
             Authorization: `Bearer ${token}`,
@@ -15,7 +15,7 @@ export const getSpotifyTrack = async () => {
 
 export const getTracksInAlbum = async (id) => {
     const url = `${apiUrl}albums/${id}`
-    const token = 'BQC1BuNHFGLMm38VnZkRzIWjahhHskkllU9w-WZYsu5qQibjsVbwx7T-eHwC_IcVSXPBNRzpzcZt3HtMb2dFg219_rO5FBQ6Q5HfnmokBQBrKu0sRkHXIwRCkLs0eix_Kq1LEoQRxwQPGrXjKPF0_xE3U7i3fOcJN7-J6cUa3sWfabaDJRLNLTlvbVkH86Fq'
+    const token = 'BQDCQRcdlFrHGTnrGmKZ2Uahaivpu9-HZf8iyVnS1qWKu8b6ZaGiZanMaKbnD5FBPkN_Vgo-IXi-WLzLmABKUdogzTqNcEn7a_S4wkqwVGMx-j7ETI_cDYK4J0Jx_fJdJy8EMhaLOYJa4LEldIyDvNVDV3mkRiUmWIc5-STkAXaJ6iD_fL6W9tnREfSBrzBa'
     const config = {
         headers: {
             Authorization: `Bearer ${token}`,
