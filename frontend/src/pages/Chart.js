ab0d5df426a1905ff950cb0bc3ca9fa127ac5e33
Luke <80142097+LukeRykta@users.noreply.github.com>
2022-11-10 23:36:02 -0600

    updated navbar cleaned code

--- frontend/src/pages/Chart.js
+++ frontend/src/pages/Chart.js
@@ -9,15 +9,18 @@ function Chart() {
     return (
         <div className="App">
             <header className="App-header">
-                <h1>Drake</h1>
-                <div>
-                    <div className="chartContainer" style={{paddingBottom: 25, width: window.outerWidth / 1.5}}>
-                        <LineChart/>
+
+                <div className="box-shadow-test">
+                    <h1 className="text-center">Drake</h1>
+                    <div>
+                        <div style={{paddingBottom: 25, width: window.outerWidth / 1.5}}>
+                            <LineChart/>
+                        </div>
                     </div>
+                    <Link to="/">
+                        <Button className="btn-danger"> Go to Home </Button>
+                    </Link>
                 </div>
-                <Link to="/">
-                    <Button className="btn-success"> Go to Home </Button>
-                </Link>
             </header>
         </div>
     );
