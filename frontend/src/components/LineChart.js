c36cbb290c14ab34e73df68b952769e2907584d6
Brandon-Price <brandon.samuel.price@gmail.com>
2022-11-10 23:38:33 -0600

    Added  useState logic for radio buttons, made dataModel more dynamic.

--- frontend/src/components/LineChart.js
+++ frontend/src/components/LineChart.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useLayoutEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import {
     CategoryScale,
     Chart as ChartJS,
@@ -12,6 +12,7 @@ import {
 } from 'chart.js';
 import {Line} from 'react-chartjs-2';
 import {ButtonGroup, ToggleButton} from "react-bootstrap";
+import chart from "../pages/Chart";
 
 ChartJS.register(
     CategoryScale,
@@ -24,36 +25,33 @@ ChartJS.register(
     Filler,
 );
 
-const song1data = ['111', '222', '333', '444', '555', '666', '777', '888', '999', '111', '111', '111'];
-const song2data = ['222', '222', '222', '222', '222', '222', '222', '222', '222', '222', '222', '222'];
-const song3data = ['333', '333', '333', '333', '333', '333', '333', '333', '333', '333', '333', '333'];
-const song4data = ['444', '444', '444', '444', '444', '444', '444', '444', '444', '444', '444', '444'];
+const song1data = ['234', '634', '823', '124', '525', '513', '129', '592', '146', '154', '472', '432'];
+const song2data = ['648', '234', '734', '845', '534', '375', '764', '235', '534', '154', '743', '273'];
+const song3data = ['156', '235', '345', '263', '624', '267', '152', '345', '845', '483', '245', '135'];
+const song4data = ['234', '345', '444', '324', '346', '976', '623', '345', '734', '346', '852', '531'];
 const allSongData = [song1data, song2data, song3data, song4data];
 
-const dataVals = [{name: "test", value: '1'}, {name: "hi", value: '2'}, {name: "brandone likes to code", value:'3'}, {name: "luk", value: '4'}]
+const songNames = [{name: "Legend", value: '0'}, {name: "Energy", value: '1'}, {name: "10 Bands", value:'2'}, {name: "Know Yourself", value: '3'}]
 
 export const view = {
     yLables: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-    mLables: ['Week 1', 'Week 2', 'Week 3', 'Week 4', 'Week 5', 'Week 6','Week 7','Week 8','Week 9','Week 10','Week 11','Week 12'],
+    mLables: ['Week 1', 'Week 2', 'Week 3', 'Week 4', 'Week 5', 'Week 6','Week 7','Week 8'],
     wLables: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 }
-//const yLables = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 export const options = {
+
     animations: {
         tension: {
             duration: 100,
-            easing: 'easeInExpo',
-            from: .2,
+            easing: 'quadratic',
+            from: .1,
             to: 0,
             loop: false
         },
     },
     responsive: true,
     plugins: {
-        filler: {
-            propogate: true
-        },
         legend: {
             labels:{
                 font: {
@@ -70,54 +68,11 @@ export const options = {
     },
 };
 
+
+
 function LineChart() {
     const [label, setLabel] = useState(view.yLables)
     const [songIndex, setSongIndex] = useState(0);
-
-    function getSet(vals){
-        return vals;
-    }
-
-    let data1 = {
-        labels: label,
-        datasets: [
-            {
-                label: 'Legend',
-                data: allSongData.map(getSet).at(songIndex),
-                borderColor: 'rgb(255, 99, 132)',
-                backgroundColor: 'rgba(255, 99, 132, 0.5)',
-                fill: 'origin',
-                tension: .4,
-                pointHitRadius: 50,
-            },
-        ],
-    };
-
-    let data2 = {
-        labels: label,
-        datasets: [
-            {
-                label: 'Energy',
-                data: song2data,
-                borderColor: 'rgb(53, 162, 235)',
-                backgroundColor: 'rgba(53, 162, 235, 0.5)',
-            },
-        ]
-    }
-
-    let data3 = {
-        labels: label,
-        datasets: [
-            {
-                label: '10 Bands',
-                data: song3data,
-                borderColor: 'rgb(47,168,40)',
-                backgroundColor: 'rgba(34,84,22,0.5)',
-            },
-        ]
-    }
-
-    const [data, setData] = useState(data1);
     const [radioValue, setRadioValue] = useState('1');
     const [songRadioValue, setSongRadioValue] = useState('1');
 
@@ -127,64 +82,47 @@ function LineChart() {
         { name: 'Week', value: '3' },
     ];
 
-    function handleClick1(e){
-        setData(data1)
+    function getSet(vals){
+        return vals;
     }
 
-    function handleClick2(e){
-        setData(data2)
-    }
+    let dataModel = {
+        labels: label,
+        datasets: [
+            {
+                label: 'Legend',
+                data: allSongData.map(getSet).at(songIndex),
+                borderColor: 'rgb(255, 99, 132)',
+                backgroundColor: 'rgba(255, 99, 132, 0.3)',
+                fill: 'origin',
+                tension: .0,
+                pointHitRadius: 100,
+            },
+        ],
+    };
 
-    function handleClick3(e){
-        setData(data3)
-    }
+    const [data, setData] = useState(dataModel);
 
-    function inc(e){
-        setSongIndex(songIndex+1);
-        console.log(songIndex)
-    }
-
-    useEffect(() => {
-        setData(() => data1)
-    }, [songIndex])
-
-    useEffect(() => {
-        console.log("radio " + radioValue + " detected");
-            setData(() => data1);
-    }, [label]);
-
-    useEffect(() => {
-        console.log("radio " + radioValue + " detected");
-        setData(() => data1);
-    }, [songIndex]);
-
-    function handleChange(e){
-        console.log("handle change reached");
+    function handleTimeChange(e){
         setRadioValue(e.currentTarget.value);
-        console.log(e.currentTarget.value)
-        if (e.currentTarget.value == 1){
+        if (e.currentTarget.value === '1'){
             setLabel(view.yLables)
-            console.log(label)
-        } else if (e.currentTarget.value == 2){
+        } else if (e.currentTarget.value === '2'){
             setLabel(view.mLables)
-            console.log(label)
-        } else if (e.currentTarget.value == 3){
+        } else if (e.currentTarget.value === '3'){
             setLabel(view.wLables)
-            console.log(label)
         }
     }
-
-    useLayoutEffect(() => {
-        //setSongRadioValue(() => songIndex)
-    }, [songIndex])
-
 
     function handleSongChange(e){
         setSongRadioValue(e.currentTarget.value)
         setSongIndex(e.currentTarget.value)
-        //setSongRadioValue(e.currentTarget.value)
-        console.log("target: " + e.currentTarget.value + ", index: " + songIndex);
+        //console.log("target: " + e.currentTarget.value + ", index: " + songIndex);
     }
+
+    useEffect(() => {
+        setData(() => dataModel)
+    }, [songIndex, songRadioValue, label])
 
     return(
         <>
@@ -199,7 +137,7 @@ function LineChart() {
                             value={radio.value}
                             className="btn-success"
                             checked={radioValue === radio.value}
-                            onChange={handleChange}
+                            onChange={handleTimeChange}
                         >
                             {radio.name}
                         </ToggleButton>
@@ -207,19 +145,15 @@ function LineChart() {
                 </ButtonGroup>
                 <div className="">
                     <Line data={data} options={options} />
-                        {/*<Button style={{marginLeft: 40}} onClick={handleClick1}>Legend</Button>*/}
-                        {/*<Button onClick={handleClick2}>Energy</Button>*/}
-                        {/*<Button onClick={handleClick3}>10 Bands</Button>*/}
-                        {/*<Button onClick={inc}>test</Button>*/}
-                    <div className="">
-                        {dataVals.map((songRadio, idx) => (
+                    <div className="m-2">
+                        {songNames.map((songRadio, idx) => (
                             <ToggleButton
                                 key={idx}
                                 id={`songRadio-${idx}`}
                                 type="radio"
                                 name="songRadio"
-                                value={songRadio.value-1}
-                                className="btn-success"
+                                value={songRadio.value}
+                                className="btn-dark m-1"
                                 checked={songRadioValue === songRadio.value}
                                 onChange={handleSongChange}
                             >
